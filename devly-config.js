2b7561b2c16a060bbdb13058c564daec97ef1d02
Ankit Jangam <ankitjangam001@gmail.com>
2026-09-04 20:10:01 +0530

    feat: pre-configure user Google Apps Script webhook URL

--- devly-config.js
+++ devly-config.js
@@ -2,9 +2,8 @@
 // This enables instant live synchronization between Mobile Phones (Riders) and Laptops (Admin).
 
 window.DEVLY_CONFIG = {
-  // Cloud Database REST Endpoint
-  // You can use a free Google Firebase Realtime Database URL, Supabase, or Google Apps Script Webhook.
-  cloudDbUrl: localStorage.getItem('devly_custom_cloud_url') || 'https://devly-partner-registry-default-rtdb.firebaseio.com/applications',
+  // Cloud Database REST Endpoint (Google Apps Script Webhook)
+  cloudDbUrl: localStorage.getItem('devly_custom_cloud_url') || 'https://script.google.com/macros/s/AKfycbwqTRkJhrxEh4GYmvWaGCiUjGPEqlmUEnLd6PGpPmNw-fiRelOMu4mebrc66aYZKBX1/exec',
   
   // Realtime Polling Interval (in ms)
   pollIntervalMs: 10000,
@@ -19,7 +18,7 @@ window.DEVLY_CONFIG = {
       this.cloudDbUrl = url.trim();
     } else {
       localStorage.removeItem('devly_custom_cloud_url');
-      this.cloudDbUrl = 'https://devly-partner-registry-default-rtdb.firebaseio.com/applications';
+      this.cloudDbUrl = 'https://script.google.com/macros/s/AKfycbwqTRkJhrxEh4GYmvWaGCiUjGPEqlmUEnLd6PGpPmNw-fiRelOMu4mebrc66aYZKBX1/exec';
     }
   }
 };
